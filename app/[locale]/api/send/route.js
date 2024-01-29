be8fca3e9348a538db24e47290f9b2b53e9dc601
Dayou23 <zdiyaa23@gmail.com>
2024-01-29 20:08:25 +0100

    29-01-2024

--- app/[locale]/api/send/route.js
+++ app/[locale]/api/send/route.js
@@ -1,17 +1,17 @@
-import { NextRequest } from "next/server";
-import { Resend } from "resend";
+// import { NextRequest } from "next/server";
+// import { Resend } from "resend";
 
-export async function GET() {
-  const resend = new Resend("re_3YQWuudw_MpAcMD5kLXqsrFRTz8XS8Xpz");
-  try {
-    const { data } = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "zdiyaa23@gmail.com",
-      subject: "Hello World",
-      html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
-    });
-    return NextRequest.json({ hi: "word" });
-  } catch (err) {
-    return NextRequest.json({ err });
-  }
-}
+// export async function GET() {
+//   const resend = new Resend("re_3YQWuudw_MpAcMD5kLXqsrFRTz8XS8Xpz");
+//   try {
+//     const { data } = await resend.emails.send({
+//       from: "onboarding@resend.dev",
+//       to: "zdiyaa23@gmail.com",
+//       subject: "Hello World",
+//       html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
+//     });
+//     return NextRequest.json({ hi: "word" });
+//   } catch (err) {
+//     return NextRequest.json({ err });
+//   }
+// }
